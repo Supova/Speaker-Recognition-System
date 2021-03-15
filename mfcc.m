716733dd9f1afd63fb1f40cb672d1870bacc19f5
Supova <73858403+Supova@users.noreply.github.com>
2021-03-15 01:47:54 -0700

    Update mfcc.m

--- mfcc.m
+++ mfcc.m
@@ -1,33 +1,44 @@
-function [output, t] = mfcc(s, fs, N, p, M) %% fix this
-% s - Audio vector (assuming 1-channel/mono-channel)
-% fs - Sampling Frequency
-% N - Number of elements in Hamming window for stft()
-% p - Number of filters in the filter bank for melfb
-% M - overlap length for stft()
+function [MFCCcoef, timeVec] = mfcc(s, fs, N, p, M) 
+% Inputs:
+%     s : speech signal
+%     fs : Sampling Frequency
+%     N : Frame length = window length
+%     p : Number of Mel filter banks
+%     M : Frame overlap length
+    
+% Outputs:
+%     MFCCcoef : MFCC coefficents 
+%     timeVec : time vector 
 
     % using MATLAB's stft function to frame, window, and take fft
-    [s,f,t] = stft(s, fs, 'Window', hamming(N), 'OverlapLength', M);
-    
-    % Mel-frequency Wrapping using filter banks
-    m = melfb(p, N, fs)
+    [s,freqVec,timeVec] = stft(s, fs, 'Window', hamming(N), 'OverlapLength', M)
     
     % taking the positive half of the stft due to symmetry
-    s = s((N/2):end, :)
+    s = s((N/2):end, :);
     
-    % convert amplitude to dB 
-    s = mag2db(abs(s));
+    % take absolute value 
+    s = abs(s);
+   
+   %plot(s)
+    
+    % Mel-frequency Wrapping using filter banks
+    m = melfb(p, N, fs);
     
     % mel filter bank output
-    mel_output = m*s
+    mel_output = m*s;
+    
+    % 
+    log_mel_output = log(mel_output);
     
     % discrete cosine transform
-    output = dct(mel_output)
+    MFCCcoef = dct(log_mel_output);
     
-    
+    % exclude 0'th order cepstral coefficient as it's the mean value 
+    % (doesn't provide much info)
+    %  output(1,:) = [];    
    
+%    consider whether to do scaling here
     
-    % Step 6: Plot the amplitude output of the dct
-    %plotSpec(ystt, 1:p, cn); caxis([-30 15]);
-    %xlim([min(ystt), max(ystt)]); ylim([1 p]);
-    %xlabel('Time (s)'); ylabel('mfc coefficients')
+% any plotting neeed
+
 end
