215b494071b5934ff59f3ebe31abf6f3e12e45bf
Supova <73858403+Supova@users.noreply.github.com>
2021-03-15 01:49:26 -0700

    Update main.m

--- main.m
+++ main.m
@@ -24,3 +24,27 @@ MFCC_1 = MFCC_1 ./ max(max(abs(MFCC_1))
 
 % plot ceptral coefficents 
 plot_ceptrum(timeVec_1, MFCC_1, p, 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function plot_ceptrum(timeVec, MFCCcoef, p, speaker_id)
+    figure; 
+    surf(timeVec, 1:p, MFCCcoef,'EdgeColor','none'); 
+    view(0, 90); 
+    colorbar;
+    xlim([min(timeVec), max(timeVec)]); 
+    ylim([1 p]);
+    xlabel('Time (s)'); ylabel('Ceptral Coefficients');
+    title(strcat('Speaker: ', int2str(speaker_id)) );
+end
