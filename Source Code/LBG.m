c5c607ca68d998b31262a462eae6fa3b8ab92a4b
Supova <73858403+Supova@users.noreply.github.com>
2021-03-16 10:46:11 -0700

    Create LBG.m

---
+++ Source Code/LBG.m
@@ -0,0 +1,53 @@
+function codebook = LBG(mfcc_coeff, num_centroids)
+
+epsilon = 0.01;
+
+% initializing single-vector codebook
+codebook = mean(mfcc_coeff,2);
+centroid = 1;
+D = 1;
+
+while centroid < num_centroids
+    
+    % doubling size of codebook
+    temp = zeros([length(codebook), centroid*2]);
+    
+    % calculating yn+ and yn-
+    if centroid == 1
+            temp = [codebook*(1+epsilon), codebook*(1-epsilon)];
+    else   
+            for i = 0:range(centroid)
+                temp = [codebook(:,i) * (1+epsilon), codebook(:,i) * (1-epsilon)];   
+            end 
+    end 
+    
+    codebook = temp;
+    centroid = size(codebook);
+    
+end 
+
+d = disteu(mfcc_coeff, codebook);
+
+while abs(D) > epsilon
+    
+    % nearest neighbour search
+    prev_distance = mean(d);
+    nearest_codebook = min(d, [], 2);
+    
+    % cluster vectors and find new centroid
+    for i = 0:range(centroid)
+        codebook(:,i) = mean(mfcc_coeff(:, nearest_codebook == i), 2);
+    end 
+      
+    % updating centroid
+    % replace all NaN values with 0
+    codebook(isnan(codebook)) = 0;
+    
+    d = disteu(mfcc_coeff, codebook);
+    
+    % computing distortion            
+    D = (prev_distance - mean(d))/prev_distance;
+    
+end
+           
+end
