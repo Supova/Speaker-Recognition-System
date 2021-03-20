ca90df91490f77d2acc5d01c57fb408534886c84
Supova <73858403+Supova@users.noreply.github.com>
2021-03-20 01:39:05 -0700

    Update test.m

--- Source Code/test.m
+++ Source Code/test.m
@@ -1,37 +1,43 @@
 function test(testdir, n, code, N, p, M)
-% Speaker Recognition: Testing Stage
-%
 % Input:
 %       testdir : string name of directory contains all test sound files
 %       n       : number of test files in testdir
 %       code    : codebooks of all trained speakers
-%
+%       N       : Frame length = window length
+%       p       : Number of Mel filter banks
+%       M       : Frame overlap length
+
 % Note:
 %       Sound files in testdir is supposed to be: 
 %               s1.wav, s2.wav, ..., sn.wav
 %
 % Example:
-%       >> test('C:\data\test\', 8, code);
+%       >> test('C:\Audio\Test\', 11);
 
-for k = 1:n                     % read test sound file of each speaker
+% read test sound file of each speaker
+for k = 1:n                    
     file = sprintf('%ss%d.wav', testdir, k);
     [s, fs] = audioread(file);      
-        
-    v =  mfcc(s, fs, N, p, M);             % Compute MFCC's
+    
+     % Compute MFCC
+    v =  mfcc(s, fs, N, p, M);            
    
     distmin = inf;
     k1 = 0;
    
-    for l = 1:length(code)      % each trained codebook, compute distortion
+    % compute distortion for each trained codebook
+    for l = 1:length(code)     
         d = disteu(v, code{l}); 
         dist = sum(min(d,[],2)) / size(d,1);
       
+        % compare distance with threshold
         if dist < distmin
             distmin = dist;
             k1 = l;
         end      
     end
  
+    % output results
     msg = sprintf('Speaker ID: %d matches with ID: %d', k, k1);
     disp(msg);
 end
