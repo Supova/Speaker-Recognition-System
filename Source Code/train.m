cbaa6bcfa76a41eac45a72c4f1a4d1aea7061e31
Supova <73858403+Supova@users.noreply.github.com>
2021-03-20 01:34:31 -0700

    Update train.m

--- Source Code/train.m
+++ Source Code/train.m
@@ -1,9 +1,10 @@
-function code = train(traindir, n, N, p, M)
+function code = train(traindir,n, N, p, M)
 % Speaker Recognition: Training Stage
 %
 % Input:
 %       traindir : string name of directory contains all train sound files
 %       n        : number of train files in traindir
+%       m        : number of centroids required
 %
 % Output:
 %       code     : trained VQ codebooks, code{i} for i-th speaker
@@ -11,18 +12,23 @@ function code = train(traindir, n, N, p, M)
 % Note:
 %       Sound files in traindir is supposed to be: 
 %                       s1.wav, s2.wav, ..., sn.wav
-% Example:
-%       >> code = train('C:\data\train\', 8);
+% Usage:
+%       >> code = train('C:\Audio\Training\', 11);
 
-k = 16;                         % number of centroids required
+% number of centroids required
+m = 10;
 
-for i = 1:n                     % train a VQ codebook for each speaker
+% train a VQ codebook for each speaker
+for i = 1:n                     
     file = sprintf('%ss%d.wav', traindir, i);           
     disp(file)
    
+    % read in all audio files
     [s, fs] = audioread(file);
     
-    v = mfcc(s, fs, N, p, M);            % Compute MFCC's
-   
-    code{i} = vqlbg(v, k);      % Train VQ codebook
+    % Compute MFCC
+    v = mfcc(s, fs, N, p, M); 
+    
+    % Train VQ codebook
+    code{i} = LBG(v, m);      
 end
